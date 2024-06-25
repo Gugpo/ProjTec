64d14fa455bf0ce34a4423871186b3b15a61fd9d
Gugpo <poliveiragpo@gmail.com>
2024-06-25 19:29:58 -0300

    creation of automatic email triggering

--- ProjTec-XPInc/Program.cs
+++ ProjTec-XPInc/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ProjTec_XPInc.DataContext;
-using ProjTec_XPInc.Services;
 using ProjTec_XPInc.Services.ClientService;
+using ProjTec_XPInc.Services.EmailSchedulerService;
 using ProjTec_XPInc.Services.InvestmentManagementService;
 using ProjTec_XPInc.Services.SendEmailService;
 using System.Reflection;
